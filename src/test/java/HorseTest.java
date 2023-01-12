ae39077ef8fc4c0c87a96849994502e138af71f5
rustekapl <rustekapl@gmail.com>
2023-01-12 16:01:47 +0200

    Implement HorseTest/whenHorseNameIsBlank

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,15 +1,26 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
     @Test
-    void whenHorseNameIsNull(){
+    void whenHorseNameIsNull() {
         Throwable exception = assertThrows(
                 IllegalArgumentException.class,
-                () -> new Horse(null,50,50));
+                () -> new Horse(null, 50, 50));
 
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"\n", "\t", "\r"})
+    void whenHorseNameIsBlank(String name) {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Horse(name, 50, 50));
+
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
 }
