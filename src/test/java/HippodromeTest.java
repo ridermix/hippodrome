ffc851d71cd963a781d6563905a8f6895e910f4f
rustekapl <rustekapl@gmail.com>
2023-01-13 14:56:30 +0200

    Implement HippodromeTest/checkGetWinnerReturnHorseWithMaxDistance

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -32,17 +30,17 @@ class HippodromeTest {
     void checkHorsesListInMethodGet() {
         List<Horse> horses = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
-            horses.add(new Horse("Pegasus" + i, i+1, i+1));
+            horses.add(new Horse("Pegasus" + i, i + 1, i + 1));
         }
         Hippodrome hippodrome = new Hippodrome(horses);
         List<Horse> expected = horses;
         List<Horse> actual = hippodrome.getHorses();
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
 
     }
 
     @Test
-    void checkMoveCallsForAllHorses(){
+    void checkMoveCallsForAllHorses() {
         List<Horse> horses = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
             horses.add(mock(Horse.class));
@@ -52,5 +50,17 @@ class HippodromeTest {
         for (Horse horse : horses) {
             verify(horse).move();
         }
+    }
+
+    @Test
+    void checkGetWinnerReturnHorseWithMaxDistance() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            horses.add(new Horse("Pegasus" + i, i, i));
+        }
+        Hippodrome hippodrome = new Hippodrome(horses);
+        Horse expected = horses.get(horses.size() - 1);
+        Horse actual = hippodrome.getWinner();
+        assertEquals(expected, actual);
     }
 }
