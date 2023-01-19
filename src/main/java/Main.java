6c050854daf615f4fbb332092126b444d0c30786
rustekapl <rustekapl@gmail.com>
2023-01-19 15:31:35 +0200

    implement log4j.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
+
         List<Horse> horses = List.of(
                 new Horse("Буцефал", 2.4),
                 new Horse("Туз Пик", 2.5),
@@ -14,6 +15,8 @@ public class Main {
                 new Horse("Вишня", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+
+
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
